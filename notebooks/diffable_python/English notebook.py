6c51157c0a250ab4e977bcfc7abf197269209a36
Brian <brianmkcebm@gmail.com>
2022-04-13 22:06:34 +0100

    run sql and commit results

--- notebooks/diffable_python/English notebook.py
+++ notebooks/diffable_python/English notebook.py
@@ -51,12 +51,12 @@ month,
   SUM(actual_cost) AS actual_cost,
   SUM(net_cost) AS net_cost
 FROM
-  ebmdatalab.hscic.normalised_prescribing_standard AS presc
+  ebmdatalab.hscic.normalised_prescribing_standard AS rx
   INNER JOIN hscic.ccgs AS ccg ON rx.pct = ccg.code 
 WHERE
   bnf_code LIKE "1502010J0%EL" # brand and Lidocaine patches
   AND month >= "2015-01-01"
-  AND ccg.org_type = 'CCG'
+  AND ccg.org_type = 'CCG' #restrict to only ccg orgs i.e. exclude OOH etc
 GROUP BY
    month,
    pct
